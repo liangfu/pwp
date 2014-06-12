f6fc536828455449442e263ae4ee8628d75b5eaf
Liangfu Chen <chenclf@gmail.com>
2014-06-12 08:13:52 +0800

    enable image capture;

--- android/src/com/liangfu/pwptracker/CaptureActivity.java
+++ android/src/com/liangfu/pwptracker/CaptureActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.provider.MediaStore;
 import android.database.Cursor;
 
@@ -35,5 +36,35 @@ public class CaptureActivity extends Activity
 		// 	});
 	}
 
+  private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+  private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        // Image captured and saved to fileUri specified in the Intent
+        Toast.makeText(this, "Image saved to:\n" +
+                       data.getData(), Toast.LENGTH_LONG).show();
+      } else if (resultCode == RESULT_CANCELED) {
+        // User cancelled the image capture
+      } else {
+        // Image capture failed, advise user
+      }
+    }
+
+    if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        // Video captured and saved to fileUri specified in the Intent
+        Toast.makeText(this, "Video saved to:\n" +
+                       data.getData(), Toast.LENGTH_LONG).show();
+      } else if (resultCode == RESULT_CANCELED) {
+        // User cancelled the video capture
+      } else {
+        // Video capture failed, advise user
+      }
+    }
+  }
+  
 }
 
--- android/src/com/liangfu/pwptracker/CvTracker.java
+++ android/src/com/liangfu/pwptracker/CvTracker.java
@@ -14,6 +14,11 @@ import android.content.Intent;;
 import android.provider.MediaStore;
 import android.database.Cursor;
 import android.net.Uri;
+import android.widget.Toast;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.os.Environment;
 
 import com.liangfu.pwptracker.R;
 
@@ -23,7 +28,15 @@ public class CvTracker extends Activity
   private static final String TAG            = "CompVis::EntryActivity";
 
 	private static final int SELECT_PICTURE = 1;
+  private static final int REQUEST_IMAGE_CAPTURE = 1;
 
+  public static final int MEDIA_TYPE_IMAGE = 1;
+  public static final int MEDIA_TYPE_VIDEO = 2;
+  
+  private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+  private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+  private Uri fileUri;
+  
 	private String selectedImagePath;
 	
   // public static final int     VIEW_MODE_RGBA = 0;
@@ -68,6 +81,90 @@ public class CvTracker extends Activity
 		// startActivity(intent);
 	}
 
+  public void dispatchTakePictureIntent(View v) {
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    // if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+    //   startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    // }
+    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+  }
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+  {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == SELECT_PICTURE) {
+				Uri selectedImageUri = data.getData();
+				selectedImagePath = getPath(selectedImageUri);
+				Intent intent = new Intent(this, EditorActivity.class);
+				intent.putExtra("selectedImagePath",selectedImagePath);
+				startActivity(intent);
+			}
+		}
+
+    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        // Image captured and saved to fileUri specified in the Intent
+        Toast.makeText(this, "Image saved to:\n" +
+                       data.getData(), Toast.LENGTH_LONG).show();
+      } else if (resultCode == RESULT_CANCELED) {
+        // User cancelled the image capture
+      } else {
+        // Image capture failed, advise user
+      }
+    }
+
+    if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        // Video captured and saved to fileUri specified in the Intent
+        Toast.makeText(this, "Video saved to:\n" +
+                       data.getData(), Toast.LENGTH_LONG).show();
+      } else if (resultCode == RESULT_CANCELED) {
+        // User cancelled the video capture
+      } else {
+        // Video capture failed, advise user
+      }
+    }
+	}
+
+  private static Uri getOutputMediaFileUri(int type){
+    return Uri.fromFile(getOutputMediaFile(type));
+  }
+
+  private static File getOutputMediaFile(int type){
+    // To be safe, you should check that the SDCard is mounted
+    // using Environment.getExternalStorageState() before doing this.
+
+    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_PICTURES), "MyCameraApp");
+    // This location works best if you want the created images to be shared
+    // between applications and persist after your app has been uninstalled.
+
+    // Create the storage directory if it does not exist
+    if (! mediaStorageDir.exists()){
+      if (! mediaStorageDir.mkdirs()){
+        Log.d("MyCameraApp", "failed to create directory");
+        return null;
+      }
+    }
+
+    // Create a media file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    File mediaFile;
+    if (type == MEDIA_TYPE_IMAGE){
+      mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                           "IMG_"+ timeStamp + ".jpg");
+    } else if(type == MEDIA_TYPE_VIDEO) {
+      mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                           "VID_"+ timeStamp + ".mp4");
+    } else {
+      return null;
+    }
+
+    return mediaFile;
+  }
+  
   // @Override
   // public boolean onCreateOptionsMenu(Menu menu) {
   //   Log.i(TAG, "onCreateOptionsMenu");
@@ -86,18 +183,6 @@ public class CvTracker extends Activity
   //   }
   //   return true;
   // }
-
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			if (requestCode == SELECT_PICTURE) {
-				Uri selectedImageUri = data.getData();
-				selectedImagePath = getPath(selectedImageUri);
-				Intent intent = new Intent(this, EditorActivity.class);
-				intent.putExtra("selectedImagePath",selectedImagePath);
-				startActivity(intent);
-			}
-		}
-	}
 
 	/**
 	 * helper to retrieve the path of an image URI

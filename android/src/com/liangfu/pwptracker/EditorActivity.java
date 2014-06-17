c005448372ae851022193bcc22199d045987eb5f
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2014-06-17 18:25:13 +0800

    implemented mask drawing function; added apply and save button image;

--- android/src/com/liangfu/pwptracker/EditorActivity.java
+++ android/src/com/liangfu/pwptracker/EditorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.View;
 import android.widget.Button;
 import android.provider.MediaStore;
@@ -13,6 +14,8 @@ import android.widget.TextView;
 import android.view.MotionEvent;
 import android.util.Log;
 import android.graphics.BitmapFactory;
+import android.util.FloatMath;
+import android.graphics.PointF;
 
 import com.liangfu.widgets.GestureImageView;
 
@@ -20,15 +23,32 @@ import com.liangfu.pwptracker.R;
 
 public class EditorActivity extends Activity 
 {
+	private static final String TAG="EditorActivity";
 	private String selectedImagePath;
-	private GestureImageView view=null;
+
 	private Button eraserButton=null;
 	private Button regionButton=null;
+	private EditorImageView editorImageView=null;
+
+	private static final int NONE = 0;
+	
+	private int mMode = NONE;
+	private static final int DRAG = 1;
+	private static final int ZOOM = 2;
+	private static final int DRAW = 3;
+
+	private int mPressed = NONE;
+	private static final int ERASER = 1;
+	private static final int REGION = 2;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.editor);
+
+		eraserButton=((Button)findViewById(R.id.eraserButton));
+		regionButton=((Button)findViewById(R.id.regionButton));
+		editorImageView=((EditorImageView)findViewById(R.id.editorImageView));
 
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
@@ -38,18 +58,16 @@ public class EditorActivity extends Activity
 			setImagePath(path);
 		}
 
-		eraserButton=((Button)findViewById(R.id.eraserButton));
-		regionButton=((Button)findViewById(R.id.regionButton));
-		eraserButton.setOnClickListener(new OnClickListener(){public void onClick(View v){
-			
-		}});
+		eraserButton.setOnTouchListener(new EraserButtonOnTouchListener());
+		regionButton.setOnTouchListener(new RegionButtonOnTouchListener());
+		editorImageView.setOnTouchListener(new EditorImageViewOnTouchListener());
+		
 	}
 
 	public void setImagePath(String imagePath)
 	{
 		selectedImagePath=imagePath;
-		EditorImageView v=((EditorImageView)findViewById(R.id.editorImageView));
-		v.setImagePath(imagePath);
+		editorImageView.setImagePath(imagePath);
 
 		// view=((GestureImageView)findViewById(R.id.gestureImageView));
 		// view.setImageBitmap(BitmapFactory.decodeFile(imagePath));
@@ -64,5 +82,120 @@ public class EditorActivity extends Activity
 	// 	return true;
 	// }
 
+	private class EraserButtonOnTouchListener implements View.OnTouchListener
+	{
+		@Override
+			public boolean onTouch(View v, MotionEvent event){
+			// show interest in events resulting from ACTION_DOWN
+			if(event.getAction()==MotionEvent.ACTION_DOWN) { return true; }
+			// don't handle event unless its ACTION_UP so "doSomething()" only runs once.
+			if(event.getAction()!=MotionEvent.ACTION_UP) { return false; }
+			boolean pressed=eraserButton.isPressed();
+			if (!pressed){
+				eraserButton.setPressed(true);
+				mPressed=ERASER;
+			}else{
+				eraserButton.setPressed(false);
+				mPressed=NONE;
+			}
+			Log.d(TAG,String.format("status %d",mPressed));
+			return true;
+		}
+	}
+
+	private class RegionButtonOnTouchListener implements View.OnTouchListener
+	{
+		@Override
+			public boolean onTouch(View v, MotionEvent event){
+			// show interest in events resulting from ACTION_DOWN
+			if(event.getAction()==MotionEvent.ACTION_DOWN) { return true; }
+			// don't handle event unless its ACTION_UP so "doSomething()" only runs once.
+			if(event.getAction()!=MotionEvent.ACTION_UP) { return false; }
+			boolean pressed=regionButton.isPressed();
+			if (!pressed){
+				regionButton.setPressed(true);
+				mPressed=REGION;
+			}else{
+				regionButton.setPressed(false);
+				mPressed=NONE;
+			}
+			Log.d(TAG,String.format("status %d",mPressed));
+			return true;
+		}
+	}
+
+	private class EditorImageViewOnTouchListener implements View.OnTouchListener
+	{
+		private PointF mStartPoint = new PointF();
+		private PointF mMidPoint = new PointF();
+
+		private float mOldDistance;
+		
+		private float calcDistance(MotionEvent event) {
+			float x = event.getX(0) - event.getX(1);
+			float y = event.getY(0) - event.getY(1);
+			return FloatMath.sqrt(x * x + y * y);
+		}
+
+		private void calcMidPoint(PointF midPoint, MotionEvent event) {
+			float x = event.getX(0) + event.getX(1);
+			float y = event.getY(0) + event.getY(1);
+			midPoint.set(x / 2, y / 2);
+		}
+		
+		public boolean onTouch(View v, MotionEvent event) {
+			Log.d(TAG,String.format("status %d",mPressed));
+			switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+			case MotionEvent.ACTION_DOWN:
+				{
+					if (mPressed==NONE) {
+						mMode = DRAG;
+						mStartPoint.set(event.getX(), event.getY());
+						editorImageView.mSavedMatrix.set(editorImageView.mMatrix);
+					}else
+					{
+						mMode = DRAW;
+					}
+				}
+				break;
+
+			case MotionEvent.ACTION_MOVE:
+				if(mMode == DRAG) {
+					editorImageView.mMatrix.set(editorImageView.mSavedMatrix);
+					float x = event.getX() - mStartPoint.x;
+					float y = event.getY() - mStartPoint.y;
+					editorImageView.mMatrix.postTranslate(x, y);
+				} else if (mMode == ZOOM) {
+					float newDist = calcDistance(event);
+					float scale = newDist / mOldDistance;
+					editorImageView.mMatrix.set(editorImageView.mSavedMatrix);
+					editorImageView.mMatrix.postScale(scale, scale, mMidPoint.x, mMidPoint.y);
+				} else if (mMode == DRAW) {
+					editorImageView.paintMask(event.getX(),event.getY());
+				}
+				break;
+
+			case MotionEvent.ACTION_UP:
+				mMode = NONE;
+				break;
+
+			case MotionEvent.ACTION_POINTER_DOWN:
+				mMode = ZOOM;
+				mOldDistance = calcDistance(event);
+				calcMidPoint(mMidPoint, event);
+				break;
+
+			case MotionEvent.ACTION_POINTER_UP:
+				mMode = NONE;
+				break;
+			default:
+				break;
+			}
+
+			editorImageView.setImageMatrix(editorImageView.mMatrix);
+			return true;
+		}
+	}
 }
 
--- android/src/com/liangfu/pwptracker/EditorImageView.java
+++ android/src/com/liangfu/pwptracker/EditorImageView.java
@@ -1,9 +1,8 @@
 package com.liangfu.pwptracker;
 
-import android.content.Context;
-// import android.view.SurfaceHolder;
-import android.widget.ImageView;
 import java.util.ArrayList;
+import android.content.Context;
+import android.widget.ImageView;
 import android.util.AttributeSet;
 import android.graphics.Color;
 import android.graphics.Bitmap;
@@ -12,7 +11,6 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.Matrix;
 import android.graphics.RectF;
-import android.graphics.PointF;
 import android.view.animation.Animation;
 import android.view.ScaleGestureDetector;
 import android.view.GestureDetector;
@@ -21,14 +19,16 @@ import android.widget.TextView;
 import android.view.View;
 import android.util.Log;
 import android.util.FloatMath;
+import android.graphics.PointF;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Transformation;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.Paint;
 import android.content.res.Resources;
-
-//import com.liangfu.widgets.GestureImageView;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
 
 import com.liangfu.pwptracker.R;
 
@@ -38,6 +38,11 @@ public class EditorImageView extends ImageView
 	public static final String TAG="EditorImageView";
 
 	private ArrayList<Bitmap> mLayers;
+
+	private LayerDrawable mLayerDrawable=null;
+	// private Drawable mMaskLayer=null;
+	// private Drawable mImageLayer=null;
+	// private Drawable [] mLayersHolder = new Drawable[2];
 
 	private Bitmap mBitmap=null;
 	private String m_imagePath="";
@@ -63,21 +68,10 @@ public class EditorImageView extends ImageView
 	// private float pivotPointX = 0f;
 	// private float pivotPointY = 0f;
 
-	private Matrix mMatrix = new Matrix();
-	private Matrix mSavedMatrix = new Matrix();
+	public Matrix mMatrix = new Matrix();
+	public Matrix mSavedMatrix = new Matrix();
 
-	private int mMode = NONE;
-	private static final int NONE = 0;
-	private static final int DRAG = 1;
-	private static final int ZOOM = 2;
-	private static final int DRAW = 3;
-
-	private PointF mStartPoint = new PointF();
-	private PointF mMidPoint = new PointF();
-	private float mOldDistance;
-
-	private boolean mFitFlag = false;
-
+	public boolean mFitFlag = false;
 
 	public EditorImageView(Context context) {
 		super(context);
@@ -98,36 +92,84 @@ public class EditorImageView extends ImageView
 
 		setScaleType(ImageView.ScaleType.MATRIX);
 
-		setOnTouchListener(new EditorOnTouchListener());
-		
-		// gestureDetector = new GestureDetector(context, new GestureListener(this));
-    // mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
-
-		// borderPaint = new Paint();
-    // borderPaint.setARGB(255, 255, 128, 0);
-    // borderPaint.setStyle(Paint.Style.STROKE);
-    // borderPaint.setStrokeWidth(4);
-
-    // backgroundPaint = new Paint();
-    // backgroundPaint.setARGB(32, 255, 255, 255);
-    // backgroundPaint.setStyle(Paint.Style.FILL);
-
 		mLayers = new ArrayList<Bitmap>();
+
+		BitmapDrawable imageLayer = new BitmapDrawable();
+    imageLayer.getPaint().setColor(Color.WHITE);
+		BitmapDrawable maskLayer = new BitmapDrawable();
+		maskLayer.getPaint().setColor(Color.WHITE);
+		Drawable [] layers={maskLayer,imageLayer};
+		mLayerDrawable = new LayerDrawable(layers);
+		mLayerDrawable.setLayerInset(0,0,0,0,0);
+		mLayerDrawable.setLayerInset(1,10,0,10,10);
 	}
 
 	public void setImagePath(String imagePath)
 	{
 		m_imagePath=imagePath;
-		mBitmap=BitmapFactory.decodeFile(imagePath);
-		//mIcon = context.getResources().getDrawable(R.drawable.icon);
-		//mIcon = Drawable.createFromPath(imagePath);
+		
+		mBitmap=BitmapFactory.decodeFile(imagePath).copy(Bitmap.Config.ARGB_8888,true);
 		mLayers.add(mBitmap);
-		// mIcon.setBounds(0, 0, mIcon.getIntrinsicWidth(), mIcon.getIntrinsicHeight());
-		// float w=mBitmap.getWidth() *getResources().getDisplayMetrics().density;
-		// float h=mBitmap.getHeight()*getResources().getDisplayMetrics().density;
-		//mIcon.setBounds(0, 0, Math.round(w), Math.round(h));
 
+		// mLayerDrawable.setDrawableByLayerId(0,Drawable.createFromPath(imagePath));
+		// mLayerDrawable.setDrawableByLayerId(1,Drawable.createFromPath(imagePath));
+		mLayerDrawable.setDrawableByLayerId(mLayerDrawable.getId(0),
+																				new BitmapDrawable(getResources(),mBitmap));
+		mLayerDrawable.setDrawableByLayerId(mLayerDrawable.getId(1),
+																				new BitmapDrawable(getResources(),mBitmap));
+		// ((BitmapDrawable)mLayerDrawable.getDrawable(0)).setBitmap(mBitmap);
+		// ((BitmapDrawable)mLayerDrawable.getDrawable(1)).setBitmap(mBitmap);
+		
 		mFitFlag=true;
+	}
+
+	public static Bitmap drawableToBitmap (Drawable drawable) {
+    if (drawable instanceof BitmapDrawable) {
+			return ((BitmapDrawable)drawable).getBitmap();
+    }
+
+    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), 
+																				drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap); 
+    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    drawable.draw(canvas);
+
+    return bitmap;
+	}
+
+	public void paintMask(float x, float y)
+	{
+		//Bitmap xx=((BitmapDrawable)mLayerDrawable.getDrawable(0)).getBitmap();
+		// Bitmap xx=drawableToBitmap(mLayerDrawable.getDrawable(0));
+		// if (xx==null){Log.d(TAG,String.format("xx is null"));}
+		// Bitmap bitmap = mBitmap.copy(Bitmap.Config.ARGB_8888,true);
+		//((BitmapDrawable)mLayerDrawable.getDrawable(0)).getBitmap();
+		// ....
+		//bitmap.setPixel((int)x,(int)y,Color.RED);
+		// Canvas canvas=new Canvas(bitmap);
+		Matrix inverse=new Matrix(mMatrix);
+		if (mMatrix.invert(inverse)){Log.d(TAG,"inverted");}else{Log.d(TAG,"not inverted");}
+		
+		float [] pts={x,y};
+		inverse.mapPoints(pts);
+		x=pts[0];y=pts[1];
+
+		Paint paint = new Paint();
+		paint.setColor(Color.RED);
+		paint.setStrokeWidth(3);
+		Bitmap b = mLayers.get(0);
+		Canvas canvas2=new Canvas(b);
+		canvas2.drawRect((int)x, (int)y, (int)(x+5), (int)(y+5), paint);
+		mLayers.set(0,b);
+		invalidate();
+		//canvas2.drawRect(30, 30, 80, 80, paint);
+		//canvas.drawLine(1,1,100,100,paint);
+		// mBitmap=bitmap.copy(Bitmap.Config.ARGB_8888,true);
+		// mLayerDrawable.setDrawableByLayerId(mLayerDrawable.getId(0),
+		// 																		new BitmapDrawable(getResources(),bitmap));
+		// mLayerDrawable.setDrawableByLayerId(mLayerDrawable.getId(1),
+		// 																		new BitmapDrawable(getResources(),bitmap));
+		Log.d(TAG,String.format("paintMask (%d,%d)",(int)x,(int)y));
 	}
 
 	private void scaleToFit()
@@ -140,18 +182,6 @@ public class EditorImageView extends ImageView
 		m.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
 		mMatrix.set(m);
 		setImageMatrix(m);
-	}
-
-	private float calcDistance(MotionEvent event) {
-		float x = event.getX(0) - event.getX(1);
-		float y = event.getY(0) - event.getY(1);
-		return FloatMath.sqrt(x * x + y * y);
-	}
-
-	private void calcMidPoint(PointF midPoint, MotionEvent event) {
-		float x = event.getX(0) + event.getX(1);
-		float y = event.getY(0) + event.getY(1);
-		midPoint.set(x / 2, y / 2);
 	}
 
 	// @Override
@@ -169,181 +199,33 @@ public class EditorImageView extends ImageView
 		super.onDraw(canvas);
 
 		if (mFitFlag){scaleToFit();mFitFlag=false;}
-        
+
+		Paint paint = new Paint();
+		paint.setColor(Color.RED);
+		paint.setStrokeWidth(3);
+
 		Matrix matrix = getImageMatrix();
+		
 		if (matrix != null) {
 			int numLayers = mLayers.size();
 			for (int i = 0; i < numLayers; i++) {
 				Bitmap b = mLayers.get(i);
+				// Canvas canvas2=new Canvas(b);
+				// canvas2.drawRect(30, 30, 80, 80, paint);
 				canvas.drawBitmap(b, matrix, null);
+				// draw(canvas,mLayerDrawable,matrix,255);
+				// setImageDrawable(mLayerDrawable);
 			}
 		}
-
-		// canvas.save();
-		// canvas.translate(mPosX, mPosY);
-		// canvas.scale(mScaleFactor, mScaleFactor);
-		// mIcon.draw(canvas);
-		// canvas.restore();
-
-		// if (canvas!=null){
-		// 	canvas.drawColor(Color.BLACK);
-		// 	if (mBitmap!=null){
-		// 		// setImageBitmap(mBitmap);
-		// 		// setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-		// 		Rect src = new Rect(0,0,mBitmap.getWidth(),mBitmap.getHeight());
-		// 		float imgw=mBitmap.getWidth();
-		// 		float imgh=mBitmap.getHeight();
-		// 		float w=getWidth();
-		// 		float h=getHeight();
-		// 		float xscale=w/imgw;float yscale=h/imgh;
-		// 		float scale=(xscale<=yscale)?xscale:yscale;
-		// 		long xloc=Math.round((w-imgw*scale)*.5);
-		// 		long yloc=Math.round((h-imgh*scale)*.5);
-		// 		Rect dst = new Rect((int)xloc,(int)yloc,(int)(w-xloc),(int)(h-yloc));
-		// 		canvas.drawBitmap(mBitmap,src,dst,null);
-		// 	}
-		// }
 	}
 
-	// delegate the event to the gesture detector
-	// @Override
-	// public boolean onTouchEvent(MotionEvent e) {
-	// 	return gestureDetector.onTouchEvent(e);
+	// private void draw(Canvas canvas, Drawable drawable, Matrix matrix, int alpha) {
+	// 	canvas.save(Canvas.MATRIX_SAVE_FLAG);
+	// 	canvas.concat(matrix);
+	// 	drawable.setAlpha(alpha);
+	// 	drawable.draw(canvas);
+	// 	canvas.restore();
 	// }
-
-	/*
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		// Let the ScaleGestureDetector inspect all events.
-		mScaleDetector.onTouchEvent(ev);
-    
-		final int action = ev.getAction();
-		switch (action & MotionEvent.ACTION_MASK) {
-		case MotionEvent.ACTION_DOWN: 
-			{
-				final float x = ev.getX();
-				final float y = ev.getY();
-        
-				mLastTouchX = x;
-				mLastTouchY = y;
-				mActivePointerId = ev.getPointerId(0);
-				break;
-			}
-
-		case MotionEvent.ACTION_MOVE: 
-			{
-				final int pointerIndex = ev.findPointerIndex(mActivePointerId);
-				final float x = ev.getX(pointerIndex);
-				final float y = ev.getY(pointerIndex);
-
-				// Only move if the ScaleGestureDetector isn't processing a gesture.
-				if (!mScaleDetector.isInProgress()) {
-					final float dx = x - mLastTouchX;
-					final float dy = y - mLastTouchY;
-
-					mPosX += dx;
-					mPosY += dy;
-
-					invalidate();
-				}
-
-				mLastTouchX = x;
-				mLastTouchY = y;
-
-				break;
-			}
-        
-		case MotionEvent.ACTION_UP: 
-			{
-				mActivePointerId = INVALID_POINTER_ID;
-				break;
-			}
-        
-		case MotionEvent.ACTION_CANCEL: 
-			{
-				mActivePointerId = INVALID_POINTER_ID;
-				break;
-			}
-    
-		case MotionEvent.ACTION_POINTER_UP: 
-			{
-				final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) 
-					>> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-				final int pointerId = ev.getPointerId(pointerIndex);
-				if (pointerId == mActivePointerId) {
-					// This was our active pointer going up. Choose a new
-					// active pointer and adjust accordingly.
-					final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-					mLastTouchX = ev.getX(newPointerIndex);
-					mLastTouchY = ev.getY(newPointerIndex);
-					mActivePointerId = ev.getPointerId(newPointerIndex);
-				}
-				break;
-			}
-		}
-    
-		return true;
-	}
-
-	public boolean onDoubleTap(MotionEvent e) {
-		float x = e.getX();
-		float y = e.getY();
-		Log.d("EditorImageView","Double Tapped at: (" + x + "," + y + ")");
-		Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.scale_up);
-		startAnimation(anim);
-		return true;
-	}
-	
-	@Override 
-	public void setImageDrawable(Drawable drawable) {
-    // Constrain to given size but keep aspect ratio
-    int width = drawable.getIntrinsicWidth();
-    int height = drawable.getIntrinsicHeight();
-    mLastTouchX = mPosX = 0;
-    mLastTouchY = mPosY = 0;
-
-		Log.d(TAG,"setImageDrawable:0");
-		if (borderPaint==null){return;}
-		Log.d(TAG,"setImageDrawable:1");
-
-    int borderWidth = (int) borderPaint.getStrokeWidth();
-    mScaleFactor = Math.min(((float) getLayoutParams().width - borderWidth)
-														/ width, ((float) getLayoutParams().height - borderWidth)
-														/ height);
-    pivotPointX = (((float)getLayoutParams().width - borderWidth)-(int)(width *mScaleFactor))*.5f;
-    pivotPointY = (((float)getLayoutParams().height- borderWidth)-(int)(height*mScaleFactor))*.5f;
-    super.setImageDrawable(drawable);
-	}
-
-	private class GestureListener extends GestureDetector.SimpleOnGestureListener 
-	{
-		private EditorImageView holder;
-		public GestureListener(EditorImageView v)
-		{
-			this.holder=v;
-		}
-		@Override
-		public boolean onDown(MotionEvent e) {
-			return true;
-		}
-		@Override
-		public boolean onDoubleTap(MotionEvent e) {
-			holder.onDoubleTap(e);
-			return true;
-		}
-	}
-
-	private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener 
-	{
-    @Override
-		public boolean onScale(ScaleGestureDetector detector) {
-			mScaleFactor *= detector.getScaleFactor();
-			mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
-			invalidate();
-			return true;
-    }
-	}
-	*/
 
 	/*
 	public class Layer 
@@ -417,51 +299,5 @@ public class EditorImageView extends ImageView
 	}
 	*/
 
-	private class EditorOnTouchListener implements View.OnTouchListener
-	{
-		public boolean onTouch(View v, MotionEvent event) {
-			switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-			case MotionEvent.ACTION_DOWN:
-				mMode = DRAG;
-				mStartPoint.set(event.getX(), event.getY());
-				mSavedMatrix.set(mMatrix);
-				break;
-
-			case MotionEvent.ACTION_MOVE:
-				if(mMode == DRAG) {
-					mMatrix.set(mSavedMatrix);
-					float x = event.getX() - mStartPoint.x;
-					float y = event.getY() - mStartPoint.y;
-					mMatrix.postTranslate(x, y);
-				} else if (mMode == ZOOM) {
-					float newDist = calcDistance(event);
-					float scale = newDist / mOldDistance;
-					mMatrix.set(mSavedMatrix);
-					mMatrix.postScale(scale, scale, mMidPoint.x, mMidPoint.y);
-				}
-				break;
-
-			case MotionEvent.ACTION_UP:
-				mMode = NONE;
-				break;
-
-			case MotionEvent.ACTION_POINTER_DOWN:
-				mMode = ZOOM;
-				mOldDistance = calcDistance(event);
-				calcMidPoint(mMidPoint, event);
-				break;
-
-			case MotionEvent.ACTION_POINTER_UP:
-				mMode = NONE;
-				break;
-			default:
-				break;
-			}
-
-			setImageMatrix(mMatrix);
-			return true;
-		}
-	}
 }
 
